8d0c32bd7343b9177c51706efdb88838e20b8b5b
chenyefu <chenyefu1@huawei.com>
2025-01-13 19:37:48 +0800

    on begin frame
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- runtime/platform_dispatcher.h
+++ runtime/platform_dispatcher.h
@@ -2,6 +2,7 @@
 #define KEELS_RUNTIME_PLATFORM_DISPATCHER_H_
 
 #include "flutter/lib/ui/window/platform_configuration.h"
+#include "flutter/fml/closure.h"
 
 namespace keels{
 
@@ -15,10 +16,13 @@ public:
 
     void SetPlatformConfiguration(flutter::PlatformConfiguration* config) {config_ = config;}
     flutter::PlatformConfiguration* GetPlatformConfiguration() {return config_;}
-
+    void SetOnBeginFrame(const fml::closure& fn) {on_begin_frame_ = fn;}
+    const fml::closure& GetOnBeginFrame() {return on_begin_frame_;}
 
 private:
     flutter::PlatformConfiguration* config_ = nullptr;
+    fml::closure on_begin_frame_;
+
 
 };
 
