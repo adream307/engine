d2690fc8facb962dbb4a7ffaa4cdd72199131e3d
chenyefu <chenyefu1@huawei.com>
2025-01-16 19:39:46 +0800

    fix2
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- lib/ui/helloworld.cc
+++ lib/ui/helloworld.cc
@@ -4,8 +4,8 @@ namespace keels{
 TextSpan::TextSpan(const std::u16string& text): text_(text) {
 }
 
-void build(fml::RefPtr<flutter::ParagraphBuilder> builder) {
-    
+void TextSpan::build(fml::RefPtr<flutter::ParagraphBuilder> builder) {
+    builder->addText2(text_);
 }
 
 
