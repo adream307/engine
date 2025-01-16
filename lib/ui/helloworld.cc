966bb9e1bdeabf5aaa5adb61770662b418bad362
chenyefu <chenyefu1@huawei.com>
2025-01-16 19:37:35 +0800

    hello world
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- lib/ui/helloworld.cc
+++ lib/ui/helloworld.cc
@@ -1,4 +1,14 @@
 #include "flutter/lib/ui/helloworld.h"
 namespace keels{
+
+TextSpan::TextSpan(const std::u16string& text): text_(text) {
+}
+
+void build(fml::RefPtr<flutter::ParagraphBuilder> builder) {
     
+}
+
+
+
+
 }
--- lib/ui/helloworld.h
+++ lib/ui/helloworld.h
@@ -2,13 +2,28 @@
 #define KEELS_LIB_UI_HELLO_WORLD_H_
 
 #include <string>
+#include "flutter/lib/ui/text.h"
+#include "flutter/lib/ui/platform_dispatcher.h"
+#include "flutter/lib/ui/text/paragraph_builder.h"
+#include "flutter/lib/ui/painting/picture_recorder.h"
+#include "flutter/lib/ui/painting/canvas.h"
+// #include "flutter/lib/ui/floating_point.h"
+#include "flutter/lib/ui/compositing/scene_builder.h"
+#include "flutter/lib/ui/geometry.h"
+#include "flutter/lib/ui/painting.h"
 
 namespace keels
 {
 
 class TextSpan{
 public:
-    TextSpan(const std::u16string& text,)
+    TextSpan(const std::u16string& text);
+    std::u16string& text() {return  text_;}
+    void build(fml::RefPtr<flutter::ParagraphBuilder> builder);
+
+private:
+    std::u16string text_;
+
 };
 
 
